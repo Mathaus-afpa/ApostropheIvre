7091a1cf82f4c29e0d3a4d56a8fdbad54f6d38dd
Bazurogg <bazurogg@gmail.com>
2025-01-08 16:51:53 +0100

    Update Libraire.java

--- apostrophejava/src/main/java/apostropheivre/models/Libraire.java
+++ apostrophejava/src/main/java/apostropheivre/models/Libraire.java
@@ -1,4 +1,29 @@
 package apostropheivre.models;
+
 public class Libraire {
-	public Libraire() {}
+
+	private String lib_nom;
+	private String lib_prenom;
+
+	public Libraire(Integer lib_id, String lib_nom, String lib_prenom) {
+		this.lib_nom = lib_nom;
+		this.lib_prenom = lib_prenom;
+	}
+
+	public String getLib_nom() {
+		return lib_nom;
+	}
+
+	public void setLib_nom(String lib_nom) {
+		this.lib_nom = lib_nom;
+	}
+
+	public String getLib_prenom() {
+		return lib_prenom;
+	}
+
+	public void setLib_prenom(String lib_prenom) {
+		this.lib_prenom = lib_prenom;
+	}
+	
 }
