a139a853d48f5f145955b16b17f7fdf20ac0a18c
Bazurogg <bazurogg@gmail.com>
2025-01-09 09:24:09 +0100

    Class Libraire ➔ regex ➕ DAO

---
+++ apostrophejava/src/main/java/apostropheivre/dao/LibraireDAO.java
@@ -0,0 +1,157 @@
+package apostropheivre.dao;
+
+import apostropheivre.models.Libraire;
+
+import java.sql.*;
+import java.util.Arrays;
+import java.util.List;
+
+public class LibraireDAO extends DAOgenerale<Libraire> {
+
+    @Override
+    public int create(Libraire obj) {
+
+        StringBuilder insertSQL = new StringBuilder("insert into Libraire (lib_nom, lib_prenom) values (?,?)");
+
+        try {
+
+            Connection con = BDDservice.getInstance().getConnection();
+            PreparedStatement pstmt = con.prepareStatement(insertSQL.toString(),PreparedStatement.RETURN_GENERATED_KEYS);
+            pstmt.setString(1, obj.getLib_nom());
+            pstmt.setString(2, obj.getLib_prenom());
+
+            pstmt.executeUpdate();
+
+            ResultSet rs = pstmt.getGeneratedKeys();
+            int newId=-1;
+
+            if(rs.next()) {
+                newId = rs.getInt(1);
+            }
+
+            BDDservice.getInstance().closeConnection();
+
+            return newId;
+
+        } catch (SQLException e) {
+
+            throw new RuntimeException(e);
+
+        }
+
+    }
+
+    @Override
+    public String update(Libraire obj, Integer pId) {
+
+        StringBuilder updateSQL = new StringBuilder("update Libraire set lib_nom=?, lib_prenom=? where lib_id = ?");
+
+        try {
+
+            Connection con = BDDservice.getInstance().getConnection();
+            PreparedStatement pstmt = con.prepareStatement(updateSQL.toString());
+            pstmt.setString(1, obj.getLib_nom());
+            pstmt.setString(2, obj.getLib_prenom());
+            pstmt.setInt(7, pId);
+
+            pstmt.executeUpdate();
+
+            BDDservice.getInstance().closeConnection();
+
+            return("Libraire mis à jour avec succès.");
+
+        } catch (SQLException e) {
+
+            throw new RuntimeException(e);
+
+        }
+
+    }
+
+    @Override
+    public String delete(Integer pId) {
+
+        StringBuilder deleteSQL = new StringBuilder("delete from Libraire where Lib_id=?");
+
+        try {
+            Connection con = BDDservice.getInstance().getConnection();
+
+            PreparedStatement pstmt = con.prepareStatement(deleteSQL.toString());
+            pstmt.setInt(1, pId);
+
+            pstmt.executeUpdate();
+
+            BDDservice.getInstance().closeConnection();
+
+            return ("Libraire supprimé avec succès");
+
+        } catch (SQLException e) {
+
+            throw new RuntimeException(e);
+
+        }
+
+    }
+
+    @Override
+    public Libraire find(Integer pId) {
+
+        Libraire libraire = new Libraire(null, null, null);
+
+        StringBuilder selectById = new StringBuilder("select * from libraire where lib_id=?");
+
+        try {
+
+            Connection con = BDDservice.getInstance().getConnection();
+            PreparedStatement pstmt = con.prepareStatement(selectById.toString());
+            pstmt.setInt(1,pId);
+            ResultSet resultSet = pstmt.executeQuery();
+
+            while(resultSet.next()) {
+
+                libraire.setLib_nom(resultSet.getString("Lib_nom"));
+                libraire.setLib_prenom(resultSet.getString("Lib_prenom"));
+
+            }
+
+            BDDservice.getInstance().closeConnection();
+
+            return libraire;
+
+        } catch (SQLException e) {
+
+            throw new RuntimeException(e);
+
+        }
+
+    }
+
+    @Override
+    public List findAll() {
+
+        String selectSQL = "select id_Libraire from libraire";
+
+        List<Libraire> listLib = Arrays.asList();
+
+        try {
+            Connection con = BDDservice.getInstance().getConnection();
+            Statement stmt = con.createStatement();
+            ResultSet resultSet = stmt.executeQuery(selectSQL);
+
+            while(resultSet.next()) {
+                listLib.add(find(resultSet.getInt("id_Libraire")));
+            }
+
+            BDDservice.getInstance().closeConnection();
+
+            return listLib;
+
+        } catch (SQLException e) {
+
+            throw new RuntimeException(e);
+
+        }
+
+    }
+
+}
--- apostrophejava/src/main/java/apostropheivre/models/Libraire.java
+++ apostrophejava/src/main/java/apostropheivre/models/Libraire.java
@@ -1,21 +1,33 @@
 package apostropheivre.models;
 
+import java.util.InputMismatchException;
+
 public class Libraire {
 
 	private String lib_nom;
 	private String lib_prenom;
 
-	public Libraire(Integer lib_id, String lib_nom, String lib_prenom) {
-		this.lib_nom = lib_nom;
-		this.lib_prenom = lib_prenom;
+	private final String regexNom = "^[a-zA-Zà-üÀ-Ü\\s-]+$";
+	private final String regexPrenom = "^[a-zA-Zà-üÀ-Ü\\s-]+$";
+
+	public Libraire(Integer lib_id, String lib_nom, String lib_prenom) throws InputMismatchException,
+			NullPointerException, IllegalArgumentException {
+		setLib_nom(lib_nom);
+		setLib_prenom(lib_prenom);
 	}
 
 	public String getLib_nom() {
 		return lib_nom;
 	}
 
-	public void setLib_nom(String lib_nom) {
+	public void setLib_nom(String lib_nom) throws InputMismatchException {
+
+		if (!lib_nom.matches(regexNom)) {
+			throw new InputMismatchException("Merci de saisir un nom valide.");
+		}
+
 		this.lib_nom = lib_nom;
+
 	}
 
 	public String getLib_prenom() {
@@ -23,7 +35,13 @@ public class Libraire {
 	}
 
 	public void setLib_prenom(String lib_prenom) {
+
+		if (!lib_prenom.matches(regexPrenom)) {
+			throw new InputMismatchException("Merci de saisir un prénom valide.");
+		}
+
 		this.lib_prenom = lib_prenom;
+
 	}
-	
+
 }
